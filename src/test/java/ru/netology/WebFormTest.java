b5d8223aa4f53a1eee3d1d7099338b93eb7ac2da
Aleksey Beketov <alex311271@gmail.com>
2022-12-04 20:05:18 +0300

    add new test

--- src/test/java/ru/netology/WebFormTest.java
+++ src/test/java/ru/netology/WebFormTest.java
@@ -2,6 +2,8 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -14,10 +16,15 @@ public class WebFormTest {
 
     SelenideElement form = $(".form");
 
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
 
     @Test
     void formTestIsRight() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Яак Йоала");
         form.$("[data-test-id=phone] input").setValue("+79998887755");
         form.$("[data-test-id=agreement]").click();
@@ -28,7 +35,7 @@ public class WebFormTest {
 
     @Test
     void formTestIsRight1() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Ёлкина Алёна");
         form.$("[data-test-id=phone] input").setValue("+79998887755");
         form.$("[data-test-id=agreement]").click();
@@ -39,7 +46,7 @@ public class WebFormTest {
 
     @Test
     void formTestDoubleFirstname() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Николь Мария Петрова");
         form.$("[data-test-id=phone] input").setValue("+79993332211");
         form.$("[data-test-id=agreement]").click();
@@ -50,7 +57,7 @@ public class WebFormTest {
 
     @Test
     void formTestLargeName() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Ибн Аль-Банна Аль-Марракеши");
         form.$("[data-test-id=phone] input").setValue("+79993332211");
         form.$("[data-test-id=agreement]").click();
@@ -62,7 +69,7 @@ public class WebFormTest {
 
     @Test
     void formTestNoName() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("");
         form.$("[data-test-id=phone] input").setValue("+73332221144");
         form.$("[data-test-id=agreement]").click();
@@ -73,7 +80,7 @@ public class WebFormTest {
 
     @Test
     void formTestEmptyForm() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("");
         form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement]").click();
@@ -84,7 +91,7 @@ public class WebFormTest {
 
     @Test
     void formTestNoPhone() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Антон Чехов");
         form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement]").click();
@@ -95,7 +102,7 @@ public class WebFormTest {
 
     @Test
     void formTestLatinName() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Chekhov Anton");
         form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement]").click();
@@ -106,7 +113,7 @@ public class WebFormTest {
 
     @Test
     void formTestShortPhone() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Антон Чехов");
         form.$("[data-test-id=phone] input").setValue("+7999888554");
         form.$("[data-test-id=agreement]").click();
@@ -117,7 +124,7 @@ public class WebFormTest {
 
     @Test
     void formTestLongPhone() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Федор Шаляпин");
         form.$("[data-test-id=phone] input").setValue("+799988855412");
         form.$("[data-test-id=agreement]").click();
@@ -128,7 +135,7 @@ public class WebFormTest {
 
     @Test
     void formTestNoPlusPhone() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Федор Шаляпин");
         form.$("[data-test-id=phone] input").setValue("89998885541");
         form.$("[data-test-id=agreement]").click();
@@ -139,12 +146,23 @@ public class WebFormTest {
 
     @Test
     void formTestLiterInPhone() {
-        open("http://localhost:9999");
+
         form.$("[data-test-id=name] input").setValue("Федор Шаляпин");
         form.$("[data-test-id=phone] input").setValue("+phonenumber");
         form.$("[data-test-id=agreement]").click();
         form.$("button.button").click();
         $("[data-test-id=phone].input_invalid .input__sub").shouldHave(
                 exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void formTestNotAgreement() {
+
+        form.$("[data-test-id=name] input").setValue("Яак Йоала");
+        form.$("[data-test-id=phone] input").setValue("+79998887755");
+        form.$("button.button").click();
+        $("[data-test-id=agreement].input_invalid").shouldHave(
+                exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных" +
+                        " и разрешаю сделать запрос в бюро кредитных историй"));
     }
 }
